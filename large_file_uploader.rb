2a4efdc6c513a3e3771c3ab12f9cffc94f7c146d
Kevin Sun <sunkev@brandeis.edu>
2014-06-25 12:10:16 -0400

    Fix clipboard.

--- large_file_uploader.rb
+++ large_file_uploader.rb
@@ -109,30 +109,3 @@ post '/notifications' do
   #todo: validate the hashed message saying that the upload is complete
   #todo: send email to recipient and sender confirming upload
 end
-
-def clipboard_link(text, bgcolor='#FFFFFF')
-  <<-EOF
-      <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
-              width="110"
-              height="14"
-              id="clippy" >
-      <param name="movie" value="/flash/clippy.swf"/>
-      <param name="allowScriptAccess" value="always" />
-      <param name="quality" value="high" />
-      <param name="scale" value="noscale" />
-      <param NAME="FlashVars" value="text=#{text}">
-      <param name="bgcolor" value="#{bgcolor}">
-      <embed src="/flash/clippy.swf"
-             width="110"
-             height="14"
-             name="clippy"
-             quality="high"
-             allowScriptAccess="always"
-             type="application/x-shockwave-flash"
-             pluginspage="http://www.macromedia.com/go/getflashplayer"
-             FlashVars="text=#{text}"
-             bgcolor="#{bgcolor}"
-      />
-      </object>
-  EOF
-end
