5de8cd188e5df049fd2efc0d71570a09276eb84f
Kevin Sun <sunkev@brandeis.edu>
2014-12-11 12:50:21 -0500

    Mistake commiting NEB changes.

--- large_file_uploader.rb
+++ large_file_uploader.rb
@@ -32,7 +32,7 @@ def local_request?(request_ip)
 end
 
 get '/' do
-  # pass unless local_request?(request.ip)
+  pass unless local_request?(request.ip)
   haml :internal_index
 end
 
@@ -41,12 +41,12 @@ get '/' do
 end
 
 get '/uploads/new' do
-  # pass unless local_request?(request.ip)
+  pass unless local_request?(request.ip)
   haml :new_upload
 end
 
 post '/uploads' do
-  # pass unless local_request?(request.ip)
+  pass unless local_request?(request.ip)
   sender_email = params[:sender_email].downcase
   dest_email = params[:destination_email].downcase
 
@@ -59,7 +59,6 @@ post '/uploads' do
 end
 
 get '/send/:upload_key' do |encoded_key|
-
   upload = Upload.decode(encoded_key)
 
   if upload.link_expired?
@@ -115,17 +114,19 @@ end
 
 def send_email(address, html)
   Pony.mail to: address,
-            via: :smtp,
-            subject: 'NEB Upload Ready',
-            via_options: {
-              address:               'smtp.gmail.com',
-              port:                  '587',
-              enable_starttls_auto:  true,
-              user_name:             ENV['EMAIL_ADDRESS'],
-              password:              ENV['EMAIL_PASSWORD'],
-              authentication:        :plain, # :plain, :login, :cram_md5, no auth by default
-              domain:                "localhost.localdomain" # the HELO domain provided by the client to the server
-            },
+            subject: 'NEB File Uploader: Ready to receive your files',
+            from: 'NEB File Upload Service <uploads-admin@neb.com>',
+            #via: :smtp,
+            # via_options: {
+            #     address:               'relay.neb.com',
+            #     port:                  '587',
+            #     enable_starttls_auto:  true,
+            #     openssl_verify_mode:   OpenSSL::SSL::VERIFY_NONE,
+            #     #user_name:             ENV['EMAIL_ADDRESS'],
+            #     #password:              ENV['EMAIL_PASSWORD'],
+            #     #authentication:        :plain, # :plain, :login, :cram_md5, no auth by default
+            #     domain:                "uploads.neb.com" # the HELO domain provided by the client to the server
+            # },
             html_body: erb(html)
 end
 
